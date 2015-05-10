ca2e580f5513071b3f02ab9bfe08ec2c6a7edb04
Christopher Brown <io@henrian.com>
2015-05-10 16:39:56 -0500

    Improve names tests: use parseNames instead of splitNames

--- test/names.ts
+++ test/names.ts
@@ -1,35 +1,65 @@
-/// <reference path="../type_declarations/DefinitelyTyped/node/node.d.ts" />
-/// <reference path="../type_declarations/DefinitelyTyped/mocha/mocha.d.ts" />
-import names = require('../names');
+/// <reference path="../type_declarations/index.d.ts" />
 import assert = require('assert');
 
-function testSplit(input: string, expected: string[], message?: string) {
-  var actual = names.splitNames(input);
-  assert.deepEqual(actual, expected, message);
+import types = require('../types');
+import names = require('../names');
+
+function testParse(input: string, expected: types.Name[], message?: string) {
+  it(`should parse "${input}" into expected names`, () => {
+    var actual = names.parseNames(input);
+    assert.deepEqual(actual, expected, message);
+  });
 }
 
-describe('names.splitNames', () => {
-  it('should split unswapped names', () => {
-    testSplit('David Mimno, Hanna M Wallach, and Andrew McCallum',
-       ['David Mimno', 'Hanna M Wallach', 'Andrew McCallum']);
-    testSplit('Aravind K. Joshi, Ben King and Steven Abney',
-      ['Aravind K. Joshi', 'Ben King', 'Steven Abney']);
-    testSplit('Daniel Ramage and Chris Callison-Burch',
-      ['Daniel Ramage', 'Chris Callison-Burch']);
-    testSplit('David Sankofl', ['David Sankofl']);
-    testSplit('Zhao et al.', ['Zhao', 'al.']);
+describe('Test names.splitNames:', () => {
+  describe('Unswapped names:', () => {
+    testParse('David Mimno, Hanna M Wallach, and Andrew McCallum', [
+      {first: 'David', last: 'Mimno'},
+      {first: 'Hanna', middle: 'M', last: 'Wallach'},
+      {first: 'Andrew', last: 'McCallum'},
+    ]);
+    testParse('Aravind K. Joshi, Ben King and Steven Abney', [
+      {first: 'Aravind', middle: 'K.', last: 'Joshi'},
+      {first: 'Ben', last: 'King'},
+      {first: 'Steven', last: 'Abney'},
+    ]);
+    testParse('Daniel Ramage and Chris Callison-Burch', [
+      {first: 'Daniel', last: 'Ramage'},
+      {first: 'Chris', last: 'Callison-Burch'},
+    ]);
+    testParse('David Sankofl', [
+      {first: 'David', last: 'Sankofl'},
+    ]);
+    testParse('Zhao et al.', [
+      {last: 'Zhao'},
+      {last: 'al.'},
+    ]);
   });
 
-  it('should split swapped names', () => {
+  describe('Swapped names:', () => {
     // TODO: autodetect last-name-first swaps, e.g.,
-    testSplit('Levy, R., & Daumé III, H.', ['R. Levy', 'H. Daumé III']);
-    testSplit('Chen, S., Beeferman, Rosenfeld, R.',
-      ['S. Chen', 'Beeferman', 'R. Rosenfeld']);
-    testSplit('Liu, F., Tian, F., & Zhu, Q.',
-      ['F. Liu', 'F. Tian', 'Q. Zhu']);
-    testSplit('Valtchev, V. Kershaw, D. and Odell, J.',
-      ['V. Valtchev', 'D. Kershaw', 'J. Odell']);
+    testParse('Levy, R., & Daumé III, H.', [
+      {first: 'R.', last: 'Levy'},
+      {first: 'H.', last: 'Daumé III'},
+    ]);
+    testParse('Chen, S., Beeferman, Rosenfeld, R.', [
+      {first: 'S.', last: 'Chen'},
+      {             last: 'Beeferman'},
+      {first: 'R.', last: 'Rosenfeld'},
+    ]);
+    testParse('Liu, F., Tian, F., & Zhu, Q.', [
+      {first: 'F.', last: 'Liu'},
+      {first: 'F.', last: 'Tian'},
+      {first: 'Q.', last: 'Zhu'},
+    ]);
+    testParse('Valtchev, V. Kershaw, D. and Odell, J.', [
+      {first: 'V.', last: 'Valtchev'},
+      {first: 'D.', last: 'Kershaw'},
+      {first: 'J.', last: 'Odell'},
+    ]);
     // 'Liu, Tian'; is this ['Tian Liu'] or ['Liu', 'Tian']?
-    testSplit('Liu, Tian', ['Tian Liu']);
+    // testParse('Liu, Tian', [
+    //   {first: 'Tian', last: 'Liu'},
+    // ]);
   });
 });
